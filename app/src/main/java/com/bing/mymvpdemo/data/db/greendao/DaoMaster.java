bbeb60cfc7e84114a4dc076a797d18b02cfa20e2
fengxiaobing <516668646@qq.com>
2018-01-14 18:59:31 +0800

    升级grdle版本

--- app/src/main/java/com/bing/mymvpdemo/data/db/greendao/DaoMaster.java
+++ app/src/main/java/com/bing/mymvpdemo/data/db/greendao/DaoMaster.java
@@ -21,16 +21,16 @@ public class DaoMaster extends AbstractDaoMaster {
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
-        UserDao.createTable(db, ifNotExists);
         PersonDao.createTable(db, ifNotExists);
         testDao.createTable(db, ifNotExists);
+        UserDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
-        UserDao.dropTable(db, ifExists);
         PersonDao.dropTable(db, ifExists);
         testDao.dropTable(db, ifExists);
+        UserDao.dropTable(db, ifExists);
     }
 
     /**
@@ -49,9 +49,9 @@ public class DaoMaster extends AbstractDaoMaster {
 
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
-        registerDaoClass(UserDao.class);
         registerDaoClass(PersonDao.class);
         registerDaoClass(testDao.class);
+        registerDaoClass(UserDao.class);
     }
 
     public DaoSession newSession() {
